5cb81f3e301232517ef7deca5eb92c7e858bebe1
skyciccolini <sjcf9r@umsystem.edu>
2025-09-13 10:47:43 -0500

    initialize

---
+++ Custom_Themes.R
@@ -0,0 +1,48 @@
+# custom theme for manuscript figs
+
+manuscript_theme <- list(
+  theme_classic(),
+  theme(axis.title = element_text(face = "bold", size = 12),
+        axis.text = element_text(face = "italic", size = 10),
+        text = element_text(family = "Calibri")),
+  set.seed(898760))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
