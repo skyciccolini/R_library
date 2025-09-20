760251bfcb5ff9ff5503bad7bb8753cff87d623d
skyciccolini <sjcf9r@umsystem.edu>
2025-09-20 16:32:24 -0500

    initialize

---
+++ Plot_background.R
@@ -0,0 +1,8 @@
+# remove plot background
+theme_void() +
+  theme(
+    plot.background = element_rect(fill = "transparent", color = NA),
+    panel.background = element_rect(fill = "transparent", color = NA),
+    legend.background = element_rect(fill = "transparent", color = NA),
+    legend.box.background = element_rect(fill = "transparent", color = NA)
+  )
